2ff739fffe411c0643cfae1331343ca88a5b2227
Joe Gates <115375901+randomusername87@users.noreply.github.com>
2022-12-05 15:35:15 -0500

    Update 7_state_avg_yearly.sql
    deleted old commented queries

--- 7_state_avg_yearly.sql
+++ 7_state_avg_yearly.sql
@@ -1,4 +1,4 @@
-	--What is the average number of NICS run for purchases in a YEAR for each state?
+--What is the average number of NICS run for purchases in a YEAR for each state?
 
 SELECT
 	state,
@@ -27,37 +27,3 @@ FROM
 GROUP BY state, avg_purch_monthly
 ORDER BY avg_purch_monthly DESC
 LIMIT 55;
-
-	
-	
-	
-/* old stuff below
-	
-SELECT 
-	state,
-	12 * (ROUND((total_purchase / month_count), 0)) AS avg_yearly_sales
-FROM
-	(
-	SELECT 
-		state, 
-		month_count,
-		SUM(grand_total - total_non_purchase) AS total_purchase
-	FROM
-		(
-		SELECT
-			state,
-			COUNT(month) AS month_count,
-			SUM(permit + permit_recheck  + admin + prepawn_handgun + prepawn_long_gun + prepawn_other + redemption_handgun + redemption_long_gun + redemption_other + returned_handgun + returned_long_gun + returned_other + rentals_handgun + rentals_long_gun + return_to_seller_handgun + return_to_seller_long_gun + return_to_seller_other) 
-				AS total_non_purchase,
-			SUM(totals) AS grand_total
-		FROM nics_checks
-		GROUP BY state
-		) AS sub_1
-	GROUP BY state, month_count
-	) AS sub_2
-	GROUP BY state, total_purchase, month_count
-	ORDER BY avg_yearly_sales DESC
-;
-
-
-*/
