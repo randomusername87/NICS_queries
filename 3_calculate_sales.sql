8642d1077f003b4e03fe5a7e170f7e7ff75a5ebc
Joe Gates <115375901+randomusername87@users.noreply.github.com>
2022-12-05 15:24:01 -0500

    Update 3_calculate_sales.sql
    Updating aggregation to reflect methodology explained by Buzzfeed and others.

--- 3_calculate_sales.sql
+++ 3_calculate_sales.sql
@@ -1,24 +1,23 @@
 /*SEPARATING OUT BACKGROUND CHECKS RUN FOR PURCHASES
 	(Some checks are run for various permits, and some states run checks annually 
-	to confirm permit eligibility)*/
+	to confirm permit eligibility). Used 1.1 for purchases other than multiple. Used 2 for multiple*/
 
-SELECT 
-	sub.state,
- 	non_purchase,
- 	grand_total,
- 	(grand_total - non_purchase) AS total_purchase
-FROM 
- 	(
-   	SELECT state,
-   	SUM(permit + permit_recheck  + admin + prepawn_handgun + prepawn_long_gun + prepawn_other + redemption_handgun + redemption_long_gun + redemption_other + returned_handgun + returned_long_gun + returned_other + rentals_handgun + rentals_long_gun + return_to_seller_handgun + return_to_seller_long_gun + return_to_seller_other) 
-     	AS non_purchase,
-   	SUM(totals) AS grand_total
-   	FROM nics_checks
-   	GROUP BY state
- 	) AS sub
-INNER JOIN nics_checks AS n
-ON sub.state = n.state
-GROUP BY sub.state, non_purchase, grand_total, total_purchase
-ORDER BY total_purchase DESC
+SELECT
+	state,
+	SUM(handgun_adj) + SUM(long_gun_adj) + SUM(other_adj) + SUM(multiple_adj) 
+		AS purchase_estimate
+FROM
+	(
+	SELECT 
+		state,
+		month,
+		SUM(handgun) * 1.1 AS handgun_adj,
+		SUM(long_gun) * 1.1 AS long_gun_adj,
+		SUM(other) * 1.1 AS other_adj,
+		SUM(multiple) * 2 AS multiple_adj
+	FROM nics_checks
+	GROUP BY state, month
+	) AS sub
+GROUP BY state
+ORDER BY purchase_estimate DESC
 LIMIT 55;
-
