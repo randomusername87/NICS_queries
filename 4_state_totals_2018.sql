a95394847cf5c8e6c25353463f7acc5537799bf1
Joe Gates <115375901+randomusername87@users.noreply.github.com>
2022-12-05 15:25:45 -0500

    Update 4_state_totals_2018.sql
    Updated aggregation

--- 4_state_totals_2018.sql
+++ 4_state_totals_2018.sql
@@ -1,23 +1,22 @@
---How many guns did each state buy in 2018?
+--How many guns did each state buy in 2020?
 
-SELECT 
-	sub.state,
- 	non_purchase,
- 	grand_total,
- 	(grand_total - non_purchase) AS total_purchase
-FROM 
- 	(
-   	SELECT 
+SELECT
+	state,
+	SUM(handgun_adj) + SUM(long_gun_adj) + SUM(other_adj) + SUM(multiple_adj) 
+		AS purchase_estimate
+FROM
+	(
+	SELECT 
 		state,
-   		SUM(permit + permit_recheck  + admin + prepawn_handgun + prepawn_long_gun + prepawn_other + redemption_handgun + redemption_long_gun + redemption_other + returned_handgun + returned_long_gun + returned_other + rentals_handgun + rentals_long_gun + return_to_seller_handgun + return_to_seller_long_gun + return_to_seller_other) 
-     		AS non_purchase,
-   		SUM(totals) AS grand_total
-   	FROM nics_checks
-   	WHERE month LIKE '2018%'
-   	GROUP BY state
- 	) AS sub
-INNER JOIN nics_checks AS n
-ON sub.state = n.state
-GROUP BY sub.state, non_purchase, grand_total, total_purchase
-ORDER BY total_purchase DESC
-;
+		month,
+		SUM(handgun) * 1.1 AS handgun_adj,
+		SUM(long_gun) * 1.1 AS long_gun_adj,
+		SUM(other) * 1.1 AS other_adj,
+		SUM(multiple) * 2 AS multiple_adj
+	FROM nics_checks
+	GROUP BY state, month
+	) AS sub
+WHERE month LIKE '2020%'	--change date here to examine other years
+GROUP BY state
+ORDER BY purchase_estimate DESC
+LIMIT 55;
