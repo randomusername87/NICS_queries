9c2b6c941f225b8ef9392c08a4d8a71e0ef9e1e0
Joe Gates <115375901+randomusername87@users.noreply.github.com>
2022-12-05 15:30:38 -0500

    Update 5_qty_increase_2018_2020.sql
    updated aggregation

--- 5_qty_increase_2018_2020.sql
+++ 5_qty_increase_2018_2020.sql
@@ -3,59 +3,55 @@
 WITH 
 	cte_2018 AS
 		(
-		/*cte_2018*/
-		SELECT 
-			sub18.state,
-			non_purchase,
-			grand_total,
-			(grand_total - non_purchase) AS total_purchase
-		FROM 
+		SELECT
+			state,
+			SUM(handgun_adj) + SUM(long_gun_adj) + SUM(other_adj) + SUM(multiple_adj) 
+				AS purchase_estimate
+		FROM
 			(
-		   	SELECT 
+			SELECT 
 				state,
-				SUM(permit + permit_recheck  + admin + prepawn_handgun + prepawn_long_gun + prepawn_other + redemption_handgun + redemption_long_gun + redemption_other + returned_handgun + returned_long_gun + returned_other + rentals_handgun + rentals_long_gun + return_to_seller_handgun + return_to_seller_long_gun + return_to_seller_other) 
-		     		AS non_purchase,
-		   		SUM(totals) AS grand_total
-		   	FROM nics_checks
-		   	WHERE month LIKE '2018%'
-		   	GROUP BY state
-			) AS sub18
-		JOIN nics_checks AS n
-		ON sub18.state = n.state
-		GROUP BY sub18.state, non_purchase, grand_total, total_purchase
-		ORDER BY total_purchase DESC
+				month,
+				SUM(handgun) * 1.1 AS handgun_adj,
+				SUM(long_gun) * 1.1 AS long_gun_adj,
+				SUM(other) * 1.1 AS other_adj,
+				SUM(multiple) * 2 AS multiple_adj
+			FROM nics_checks
+			GROUP BY state, month
+			) AS sub
+		WHERE month LIKE '2018%'	--change date here to compare other years
+		GROUP BY state
+		--ORDER BY purchase_estimate DESC
 		),
 	cte_2020 AS
 		(
-		/*cte_2020*/
-		SELECT 
-			sub20.state,
-			non_purchase,
-			grand_total,
-			(grand_total - non_purchase) AS total_purchase
-		FROM 
+		SELECT
+			state,
+			SUM(handgun_adj) + SUM(long_gun_adj) + SUM(other_adj) + SUM(multiple_adj) 
+				AS purchase_estimate
+		FROM
 			(
-			SELECT state,
-		   	SUM(permit + permit_recheck  + admin + prepawn_handgun + prepawn_long_gun + prepawn_other + redemption_handgun + redemption_long_gun + redemption_other + returned_handgun + returned_long_gun + returned_other + rentals_handgun + rentals_long_gun + return_to_seller_handgun + return_to_seller_long_gun + return_to_seller_other) 
-		     	AS non_purchase,
-		   	SUM(totals) AS grand_total
-		   	FROM nics_checks
-		   	WHERE month LIKE '2020%'
-		   	GROUP BY state
-		 	) AS sub20
-		INNER JOIN nics_checks AS n
-		ON sub20.state = n.state
-		GROUP BY sub20.state, non_purchase, grand_total, total_purchase
-		ORDER BY total_purchase DESC
+			SELECT 
+				state,
+				month,
+				SUM(handgun) * 1.1 AS handgun_adj,
+				SUM(long_gun) * 1.1 AS long_gun_adj,
+				SUM(other) * 1.1 AS other_adj,
+				SUM(multiple) * 2 AS multiple_adj
+			FROM nics_checks
+			GROUP BY state, month
+			) AS sub
+		WHERE month LIKE '2020%'	--change date here to compare other years
+		GROUP BY state
 		)
 SELECT
 	cte_2018.state,
-	cte_2018.total_purchase AS purchase_2018,
-	cte_2020.total_purchase AS purchase_2020,
-	(cte_2020.total_purchase - cte_2018.total_purchase) AS purchase_increase
+	cte_2018.purchase_estimate AS purchase_2018,
+	cte_2020.purchase_estimate AS purchase_2020,
+	(cte_2020.purchase_estimate - cte_2018.purchase_estimate) AS purchase_increase
 FROM cte_2018
 JOIN cte_2020
 ON cte_2018.state = cte_2020.state
-GROUP BY cte_2018.state, cte_2018.total_purchase, cte_2020.total_purchase 
+GROUP BY cte_2018.state, cte_2018.purchase_estimate, cte_2020.purchase_estimate 
 ORDER BY purchase_increase DESC
 ;
